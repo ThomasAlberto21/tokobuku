3688c44fefa482dbb50408edbfaff25ba7b07969
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-05-04 20:36:20 +0700

    .

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -36,15 +36,16 @@ class CardProduct extends React.Component {
   render() {
     return (
       <div className='lg:flex mt-10'>
-        <div className='img-thumb-prod'>
+        <div className='me-3 '>
           <img
-            src='https://www.jamstackecommerce.dev/products/couch1.png'
+            src='https://storage.googleapis.com/narasi-production.appspot.com/production/medium/1671785416672/5-rekomendasi-buku-pengembangan-diri-yang-wajib-dibaca-medium.webp'
             alt='sofa'
+            className='rounded-lg'
           />
         </div>
         <div className='mt-10'>
-          <h1 className='font-bold text-5xl'>Sofa Gray</h1>
-          <p className='font-semibold'>Rp.20.000</p>
+          <h1 className='font-bold text-5xl'>Buku Self Improvment</h1>
+          <p className='font-normal mt-2'>Rp.20.000</p>
           <p className='mt-5'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
             asperiores aliquam est dolores repudiandae suscipit provident?
