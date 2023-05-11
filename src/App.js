515c8b1741a23faaac971b22da33b37b4796c3b4
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-05-11 18:48:49 +0700

    REDUX

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from 'react';
 
-
 function App() {
-  return (
-    <div className='App px-10'>
-      
-    </div>
-  );
+  return <div className='App px-10'></div>;
 }
 
 export default App;
