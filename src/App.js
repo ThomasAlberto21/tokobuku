f5fc70689483d0fc3fe165ded57987b47c89dcac
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-05-11 18:32:38 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Product from './pages/products';
+
 
 function App() {
   return (
     <div className='App px-10'>
-      <Product />
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Product from './pages/products';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div className='App px-10'>
+    <React.StrictMode>
+      <Product />
+    </React.StrictMode>
+  </div>
 );
